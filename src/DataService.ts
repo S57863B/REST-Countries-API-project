41766616d698fb2bee663a3d20be43a0f189c892
S57863B <i349cjhswe@gmail.com>
2026-04-13 10:39:23 -0400

    fixed typo

--- src/DataService.ts
+++ src/DataService.ts
@@ -3,7 +3,7 @@ export class DataService {
   public static async fetchCountries(): Promise<Country[]> {
     try {
       const response = await fetch('./data.json');
-      if (!response.ok) throw new Error('Ошибка при загрузке данных');
+      if (!response.ok) throw new Error('Failed to fetch countries data');
       return await response.json();
     } catch (error) {
       console.error(error);
